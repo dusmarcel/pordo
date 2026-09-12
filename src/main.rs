2dc6f49ebc33cc602345e8b87e581384ef84ae80
Marcel Keienborg <marcel@aufentha.lt>
2026-09-13 00:23:16 +0200

    added clap builder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::path::PathBuf;
+
+use clap::{arg, command, value_parser};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .arg(
+            arg!(
+                <FILE> "Input file"
+            )
+            .value_parser(value_parser!(PathBuf))
+        )
+        .get_matches();
+
+    if let Some(file) = matches.get_one::<PathBuf>("FILE") {
+            println!("Hello, {}!", file.display());
+    }
 }
